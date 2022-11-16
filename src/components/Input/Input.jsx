5150751952b57be5d0e69f28ba74847024406f86
SergioTitov <sergejtitov848@gmail.com>
2022-11-16 14:57:03 +0300

    Added adaptability

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
 function Input() {
+  const [input, setInput] = useState("");
   return (
     <div>
-      <input className='input' type='text' placeholder='Add new todo' />
-      <button className='add-button'>Add</button>
+      <input
+        className='input'
+        type='text'
+        placeholder='Add new todo'
+        id='todo-input'
+        value={input}
+      />
+      <button className='add-button' id='add-button'>
+        Add
+      </button>
     </div>
   );
 }
