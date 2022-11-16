1ce68b8410441db384a1ad13d59afeec354726e0
SergioTitov <sergejtitov848@gmail.com>
2022-11-16 11:47:36 +0300

    Change color for icons

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -5,6 +5,7 @@ function Input() {
   return (
     <div>
       <input className='input' type='text' placeholder='Add new todo' />
+      <button className='add-button'>Add</button>
     </div>
   );
 }
